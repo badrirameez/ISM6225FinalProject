763f642c9c932f89e5d764f78f7a82d8f9797318
rameezbadri <rameezbadri@usf.edu>
2020-08-22 17:53:31 -0400

    Final project - initial version

---
+++ ISM6225FinalProject/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ISM6225FinalProject.Models;
+using ISM6225FinalProject.APIHandlerManager;
+using Newtonsoft.Json;
+
+namespace ISM6225FinalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Parks()
+        {
+            APIHandler webHandler = new APIHandler();
+            Places places = webHandler.GetPlaces();
+            return View(places);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ISM6225FinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ISM6225FinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
